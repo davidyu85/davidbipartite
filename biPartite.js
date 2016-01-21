64bb7d74e31938226050df0af0055a61d579d6cc
David Yu <dyu@aspingroup.com.au>
2016-01-21 11:54:28 +1000

    Reposition some headings.

--- biPartite.js
+++ biPartite.js
@@ -1,7 +1,7 @@
 !function(){
 	var bP={};	
 	var b=30, bb=480, height=600, buffMargin=8, minHeight=14;
-	var c1=[-300, 50], c2=[-40, 200], c3=[-20, 260]; //Column positions of labels.
+	var c1=[-300, 50], c2=[-60, 200], c3=[-20, 260]; //Column positions of labels.
 	var colors =["#3366CC", "#DC3912","#FF9900","#109618", "#990099", "#0099C6"];
 	
 	bP.partData = function(data,p){
